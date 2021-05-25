3e3824cffebf8f30e6f7edca51a27e4ee5acccfb
Ian Lackey <lackey_i@wustl.edu>
2021-05-25 15:58:25 -0500

    final tweaks to poc

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ template="azuredeploy.json"
 #template="azuredeploy_with_SendGrid.json"
 parameters="azuredeploy.parameters.json"
 subscription="829087ae-007e-48b5-96d9-8287ad3d00d0"
-group="itlrcpocrg4"
+group=$1
 
 loggedIn=$(az account show --query state)
 
